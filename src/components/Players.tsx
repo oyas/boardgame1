fffde9c1c75c5d0917fae70c43b950a2a1435acc
oyas <poteto83@gmail.com>
2022-03-22 01:39:54 +0900

    Modify table style

--- src/components/Players.tsx
+++ src/components/Players.tsx
@@ -1,6 +1,6 @@
 import { Box, Grid, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
 import { Game, PlayerInfo } from "../game/Game";
-import { ItemsFromId } from "../game/Items";
+import { AssemblingId, ItemsFromId, MiningId, SmeltingId } from "../game/Items";
 
 type Props = {
   game: Game;
@@ -8,21 +8,29 @@ type Props = {
 };
 
 export default function Players({ game, action }: Props) {
+  let makeRow = (id: string | number, name: string | undefined, value: string | number | undefined) => (
+    <TableRow key={id}>
+      <TableCell>{name}</TableCell>
+      <TableCell align="right" width={70}>{value}</TableCell>
+    </TableRow>
+  );
+
+  let makeRow2 = (player: PlayerInfo, id: number) =>
+    makeRow(id, ItemsFromId.get(id)?.name, new Map(player.items).get(id));
+
   let itemRows = (player: PlayerInfo) =>
-    Array.from(player.items).map(([id, count]) => (
-      <TableRow key={id}>
-        <TableCell>{ItemsFromId.get(id)?.name}</TableCell>
-        <TableCell align="right">
-          {(id == 90
-            ? (count - player.usedMining) + " / "
-            : id == 91
-            ? (count - player.usedSmelting) + " / "
-            : id == 92
-            ? (count - player.usedAssembling) + " / "
-            : "") + count}
-        </TableCell>
-      </TableRow>
-    ));
+    Array.from(player.items)
+      .filter(([id]) => id < 90)
+      .sort((a, b) => a[0] - b[0])
+      .map(([id, count], index) =>
+        makeRow(index, ItemsFromId.get(id)?.name, count)
+      );
+
+  let builder = (player: PlayerInfo, id: number, usedCount: number) => {
+    let name = "" + ItemsFromId.get(MiningId)?.name;
+    let count = new Map(player.items).get(MiningId) ?? 0;
+    return makeRow(id, name, (count - usedCount) + " / " + count);
+  }
 
   let players = game.players.map((player) => (
     <Grid item xs={6} md={3} height="100%" key={player.playerId}>
@@ -30,24 +38,36 @@ export default function Players({ game, action }: Props) {
         <Typography variant="h3">
           {(player.finished ? "✅ " : "") + player.name}
         </Typography>
-        <TableContainer>
-          <Table>
-            <TableHead></TableHead>
-            <TableBody>
-              <TableRow key="player_dice">
-                <TableCell>サイコロ</TableCell>
-                <TableCell align="right">{player.dice}</TableCell>
-              </TableRow>
-              <TableRow key="player_power">
-                <TableCell>電力</TableCell>
-                <TableCell align="right">
-                  {player.remainingEnergy} / {player.power}
-                </TableCell>
-              </TableRow>
-              {itemRows(player)}
-            </TableBody>
-          </Table>
-        </TableContainer>
+        <Grid container spacing={0}>
+          <Grid item xs={12} sm={6}>
+            <TableContainer>
+              <Table>
+                <TableBody>
+                  {makeRow("player_dice", "サイコロ", "" + player.dice)}
+                  {makeRow(
+                    "player_power",
+                    "電力",
+                    player.remainingEnergy + " / " + player.power
+                  )}
+                  {builder(player, MiningId, player.usedMining)}
+                  {builder(player, SmeltingId, player.usedSmelting)}
+                  {builder(player, AssemblingId, player.usedAssembling)}
+                  {makeRow2(player, 100)}
+                  {makeRow2(player, 101)}
+                  {makeRow2(player, 102)}
+                  {makeRow2(player, 103)}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TableContainer>
+              <Table>
+                <TableBody>{itemRows(player)}</TableBody>
+              </Table>
+            </TableContainer>
+          </Grid>
+        </Grid>
       </Box>
     </Grid>
   ));
